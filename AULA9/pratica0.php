194aa305179d2653457293db88364bb0dfa2a94c
Heron Zonta da Silva <1031390@UNIDAVI.LOCAL>
2024-10-17 21:29:33 -0300

    novas

---
+++ AULA9/pratica0.php
@@ -0,0 +1,17 @@
+<?php
+
+$salarioBruto = 0;
+$impostos = 0;
+$liquido = 0;
+
+function calcularFolhaPagto(&$bruto, &$imposto, &$liquido) {
+    $bruto = 10000;
+    $imposto = 1000;
+    $ = $bruto - $imposto
+}
+
+calcularFolhaPagto ($salarioBruto, $impostos, $liquido);
+    echo "Bruto: " . $salarioBruto . "Imposto: " . $impostos . "Líquido: " . $liquido;
+
+
+?>
